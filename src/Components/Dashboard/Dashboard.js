d1e02cbe7e8f6000de344d606bbe0615d6447f93
Thomas Berhe Ghebreyohannes <Woolu@Thomass-MacBook-Pro.local>
2019-04-05 10:58:05 -0700

    React

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import House from '../House/House'
+
+class Dashboard extends Component {
+    
+
+    render(){
+        return(
+            <div>
+                <div>Dashboard</div>
+                <div>
+                <House />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
